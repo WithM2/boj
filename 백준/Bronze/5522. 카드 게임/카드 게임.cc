a44d527a040f77e7ccd8c4557b8d66f8d17655a3
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2023-11-08 12:35:07 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <stack>
+#include <cmath>
+#include <map>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    int a;
+    int sum = 0;
+    
+    while(cin>>a) {
+        sum += a;
+    }
+    
+    cout << sum;
+    
+    
+    
+    
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
