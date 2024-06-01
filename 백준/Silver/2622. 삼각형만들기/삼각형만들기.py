b30c30604bde3d3de5f5adf1235a049b9c8a05fc
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2024-06-01 10:07:55 +0900

    [Silver V] Title: 삼각형만들기, Time: 764 ms, Memory: 110620 KB -BaekjoonHub

---
+++ 백준/Silver/2622. 삼각형만들기/삼각형만들기.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+cnt = 0
+
+for i in range(1, n):
+    for j in range(i, n):
+        k = n - i - j
+        if k < j:
+            break
+        if i + j > k:
+            cnt += 1
+
+print(cnt)
