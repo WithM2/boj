44dcb3df277b84ba3e0e854d89f0fe2ec276205e
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2024-06-29 11:08:00 +0900

    [Gold V] Title: 호숫가의 개미굴, Time: 208 ms, Memory: 157600 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
+++ 백준/Gold/28325. 호숫가의 개미굴/호숫가의 개미굴.py
@@ -1,10 +1,17 @@
 n=int(input())
 c=list(map(int,input().split()))
+
+ans=0
+
 if sum(c)!=0:
    while c[0]==0:
        d=c.pop(0)
        c.append(d)
-ans=0
+else :
+    ans += n // 2
+    print(ans)
+    exit()
+
 a=[0]*n
 b=0
 for i in c:
@@ -14,8 +21,6 @@ for i in c:
        b+=1
    ans+=i
 for i in a:
-   if sum(c)==0:
-       ans+=i//2
-   else:
        ans+=i//2+i%2
+       
 print(ans)
