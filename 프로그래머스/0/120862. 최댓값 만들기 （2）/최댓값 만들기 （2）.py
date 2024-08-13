50834d8b8b63cc4c4c1b9199a673bc6e6898c43e
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2024-08-13 14:20:58 +0900

    [level 0] Title: 최댓값 만들기 (2), Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
+++ 프로그래머스/0/120862. 최댓값 만들기 （2）/최댓값 만들기 （2）.py
@@ -1,10 +1,19 @@
 def solution(numbers):
-    answer = -999999999
+#     answer = -999999999
     
-    for i in range(len(numbers)-1):
-        for j in range(i+1, len(numbers)) :
-            if numbers[i]*numbers[j] > answer :
-                answer = numbers[i]*numbers[j]
-            
+#     for i in range(len(numbers)-1):
+#         for j in range(i+1, len(numbers)) :
+#             if numbers[i]*numbers[j] > answer :
+#                 answer = numbers[i]*numbers[j]
+
+    answer = 0
+    numbers.sort()
+    
+    answer = numbers[0]*numbers[1]
+    if numbers[-1]*numbers[-2] > answer :
+        answer = numbers[-1]*numbers[-2]
+    
+    
+    
     
     return answer
