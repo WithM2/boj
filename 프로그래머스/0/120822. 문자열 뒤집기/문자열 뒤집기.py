f9afc9c96b4d9168427d9dacae9e3d75c50f6470
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2024-08-13 14:53:47 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.py
@@ -0,0 +1,10 @@
+def solution(my_string):
+    answer = ''
+    
+    # for i in range(len(my_string)):
+    #     answer += my_string[-1-i]
+        
+    for i in range(len(my_string)-1, -1, -1) :
+        answer += my_string[i]
+    
+    return answer
