bdffff7ec52f23cd4610f8501cff48f8b630cca1
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2024-10-11 14:37:36 +0900

    [Bronze III] Title: 별 찍기 - 9, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
+++ 백준/Bronze/2446. 별 찍기 － 9/별 찍기 － 9.py
@@ -1,5 +1,11 @@
 a = int(input())
+
+
 for i in range(a):
-	print(" "*(i)+"*"*(2*(a-i-1)+1))
+    print(" "*i+"*"*(a*2-1-2*i))
+for i in range(a-2,-1,-1):
+    print(" "*i+"*"*(a*2-1-2*i))
+"""
 for i in range(a-1):
-	print(" "*(a-2-i)+"*"*(2*(i+1)+1))
+    print(" "*(a-i-2)+"*"*((i+1)*2+1))
+"""
