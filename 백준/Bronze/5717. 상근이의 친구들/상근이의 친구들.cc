fdb6e5ba46017489869ba48f6eefdcb28530907e
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2026-02-14 14:59:37 +0900

    [Bronze V] Title: 상근이의 친구들, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pii pair<int, int>
+
+int main(){
+
+    int m, f;
+    while (true) {
+        cin >> m >> f;
+        if (m == 0 && f == 0) break;
+
+        cout << m + f << "\n";
+
+
+    }
+    
+    
+
+    return 0;
+}
