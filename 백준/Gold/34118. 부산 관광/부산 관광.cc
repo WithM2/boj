565a0791fbb9688ac1b6dff39ab0cf324de6c788
Park Chan Hyeok <56295771+WithM2@users.noreply.github.com>
2026-04-25 10:02:41 +0900

    [Gold II] Title: 부산 관광, Time: 132 ms, Memory: 17896 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/34118. 부산 관광/부산 관광.cc
+++ 백준/Gold/34118. 부산 관광/부산 관광.cc
@@ -16,46 +16,55 @@ int main() {
     int p1, p3, p5, pp;
     cin >> p1 >> p3 >> p5 >> pp;
 
+    // dp[i][j] : A는 i일까지, B는 j일까지 처리한 최소 비용
     vector<vector<int>> dp(n+6, vector<int>(n+6, INF));
     dp[0][0] = 0;
 
+    // 1-index 편하게 쓰기
     vector<bool> va(n+2), vb(n+2);
     for (int i = 1; i <= n; i++) {
         va[i] = (A[i-1] == '1');
         vb[i] = (B[i-1] == '1');
     }
 
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
+    for (int i = 1; i <= n+1; i++) {
+        for (int j = 1; j <= n+1; j++) {
 
-            if (dp[i][j] == INF) continue;
-            int cur = dp[i][j];
+            int cur = dp[i-1][j-1];
+            if (cur == INF) continue;
 
-            if (i < n) {
-                if (!va[i+1]) {
-                    dp[i+1][j] = min(dp[i+1][j], cur);
-                } else {
-                    dp[i+1][j] = min(dp[i+1][j], cur + p1);
-                    dp[min(n, i+3)][j] = min(dp[min(n, i+3)][j], cur + p3);
-                    dp[min(n, i+5)][j] = min(dp[min(n, i+5)][j], cur + p5);
-                }
+            bool aa = va[i];
+            bool bb = vb[j];
+
+            // 관광 안 하는 날 처리
+            if (!aa) dp[i][j-1] = min(dp[i][j-1], cur);
+            if (!bb) dp[i-1][j] = min(dp[i-1][j], cur);
+
+            // 한국이 티켓
+            if (aa) {
+                dp[i][j-1] = min(dp[i][j-1], cur + p1);
+                for (int k = 1; k <= 3; k++)
+                    dp[min(n, i-1+k)][j-1] = min(dp[min(n, i-1+k)][j-1], cur + p3);
+                for (int k = 1; k <= 5; k++)
+                    dp[min(n, i-1+k)][j-1] = min(dp[min(n, i-1+k)][j-1], cur + p5);
             }
 
-            if (j < n) {
-                if (!vb[j+1]) {
-                    dp[i][j+1] = min(dp[i][j+1], cur);
-                } else {
-                    dp[i][j+1] = min(dp[i][j+1], cur + p1);
-                    dp[i][min(n, j+3)] = min(dp[i][min(n, j+3)], cur + p3);
-                    dp[i][min(n, j+5)] = min(dp[i][min(n, j+5)], cur + p5);
-                }
+            // 정올이 티켓
+            if (bb) {
+                dp[i-1][j] = min(dp[i-1][j], cur + p1);
+                for (int k = 1; k <= 3; k++)
+                    dp[i-1][min(n, j-1+k)] = min(dp[i-1][min(n, j-1+k)], cur + p3);
+                for (int k = 1; k <= 5; k++)
+                    dp[i-1][min(n, j-1+k)] = min(dp[i-1][min(n, j-1+k)], cur + p5);
             }
 
-            // 묶음권
-            for (int a = 0; a <= 4; a++) {
-                for (int b = 0; b <= 4; b++) {
-                    dp[min(n, i+a)][min(n, j+b)] =
-                        min(dp[min(n, i+a)][min(n, j+b)], cur + pp);
+            // 묶음권 (핵심)
+            if (i == j) {
+                for (int a = 0; a <= 4; a++) {
+                    for (int b = 0; b <= 4; b++) {
+                        dp[min(n, i-1+a)][min(n, j-1+b)] =
+                            min(dp[min(n, i-1+a)][min(n, j-1+b)], cur + pp);
+                    }
                 }
             }
         }
